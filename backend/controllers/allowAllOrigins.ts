8e4315908929cb3010034dcfebc39a93d58cd10e
Peter Byun <peter1035k@gmail.com>
2024-05-23 22:12:22 +0900

    fix(posts): allow cors requests

--- backend/controllers/allowAllOrigins.ts
+++ backend/controllers/allowAllOrigins.ts
@@ -1,8 +1,5 @@
 import { NextApiResponse } from 'next';
-import { isDev } from '../../utils/environment-checker';
 
 export function allowAllOrigins(res: NextApiResponse) {
-  if (isDev()) {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-  }
+  res.setHeader('Access-Control-Allow-Origin', '*');
 }
--- pages/api/posts/index.ts
+++ pages/api/posts/index.ts
@@ -1,6 +1,7 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import { HTTP_METHODS } from '../../../backend/constants/http.verbs';
 import { PostService } from '../../../backend/services/post.service';
+import { allowAllOrigins } from '../../../backend/controllers/allowAllOrigins';
 
 export default async function handler(
   req: NextApiRequest,
@@ -12,6 +13,7 @@ export default async function handler(
     const posts = await postService.posts({
       skip: 0,
     });
+    allowAllOrigins(res);
 
     res.json(posts);
   }
